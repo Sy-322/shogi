7ec26b6b841e6224c3ff3bdc93bf71943a6a055f
Sy-322 <syusysy2013@gmail.com>
2025-06-09 15:46:21 +0900

    Update shogi_ui.js

--- shogi_ui.js
+++ shogi_ui.js
@@ -72,6 +72,23 @@ function aiPlay() {
   render();
 }
 
+function exportKif() {
+  const data = {
+    aiDifficulty,
+    aiPersonality,
+    time: game.time,
+    history: moveHistory
+  };
+  const dataStr = JSON.stringify(data, null, 2);
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "kifu.json";
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
 // --- 関数定義 ---
 function exportKifAsKIF() {
   const lines = [];
